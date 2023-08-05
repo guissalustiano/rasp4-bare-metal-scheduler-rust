c1eac1f9bdd00e8decdd33f92e644a0d253f0a8c
Guilherme Salustiano <guissalustiano@gmail.com>
2023-08-05 20:06:23 -0300

    override panic to blink

--- src/main.rs
+++ src/main.rs
@@ -3,11 +3,10 @@
 
 pub mod timer;
 
-use core::time::Duration;
 use aarch64_cpu::asm::nop;
-
-use panic_halt as _;
 use timer::spin_for;
+
+use core::{panic::PanicInfo, time::Duration};
 use tock_registers::interfaces::Readable;
 
 // https://datasheets.raspberrypi.com/bcm2711/bcm2711-peripherals.pdf
@@ -44,4 +43,29 @@ pub extern "C" fn _start() -> ! {
 
 fn get_cpu_id() -> u64 {
     aarch64_cpu::registers::MPIDR_EL1.get() & 0b11
+}
+
+#[panic_handler]
+fn panic(_panic: &PanicInfo<'_>) -> ! {
+    unsafe {
+        // Set GPIO 42 with GPFSEL4[8:6] = 0b001
+        core::ptr::write_volatile((GPIO_START + 0x10) as *mut u32, 0b001 << 6);
+        loop {
+            // Set GPIO 42 to HIGH with GPSET1
+            core::ptr::write_volatile((GPIO_START + 0x20) as *mut u32, 1 << (42-32));
+
+            // Wait
+            for _ in 0..5000000 {
+                nop();
+            }
+
+            // Set GPIO 42 to LOW with GPCLR[42-32]
+            core::ptr::write_volatile((GPIO_START + 0x02c) as *mut u32, 1 << (42-32));
+
+            // Wait
+            for _ in 0..5000000 {
+                nop();
+            }
+        }
+    }
 }
